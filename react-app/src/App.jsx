df58578c340d473ee4f8f22ffd74b1959f37fe4f
Trofimov Max <trofimovmaxdev@gmail.com>
2022-03-28 19:00:04 +0300

    add footer content, install styled components

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import styled, { css } from 'styled-components';
 import logo from './logo.png';
 import { Routes, Route, Link } from 'react-router-dom';
 
@@ -20,6 +21,13 @@ function App() {
             <Link to="/">
               <img src={logo} alt="Logo" />
             </Link>
+          </div>
+          <div className="top-social">
+            <Link to="#"><i class="fa fa-facebook"></i></Link>
+            <Link to="#"><i class="fa fa-twitter"></i></Link>
+            <Link to="#"><i class="fa fa-google-plus"></i></Link>
+            <Link to="#"><i class="fa fa-linkedin"></i></Link>
+            <Link to="#"><i class="fa fa-instagram"></i></Link>
           </div>
           <div className="container">
             <div className="nav-menu">
@@ -60,11 +68,68 @@ function App() {
                   </Link>
                 </div>
                 <p>Despite growth of the Internet over the past seven years, the use of toll-free phone numbers
-                            in television advertising continues.</p>
+                  in television advertising continues.</p>
                 <div className="social-links">
                   <h6>Follow us</h6>
-                  <Link to="/"><i className="fa fa-instagram"></i></Link>
+                  <Link to="#"><i class="fa fa-facebook"></i></Link>
+                  <Link to="#"><i class="fa fa-twitter"></i></Link>
+                  <Link to="#"><i class="fa fa-google-plus"></i></Link>
+                  <Link to="#"><i class="fa fa-linkedin"></i></Link>
+                  <Link to="#"><i class="fa fa-instagram"></i></Link>
                 </div>
+              </div>
+            </div>
+            <div class="col-lg-3 offset-lg-1">
+              <div class="footer-widget">
+                <h5>Our Blog</h5>
+                <div class="footer-blog">
+                  <Link to="#" class="fb-item">
+                    <h6>Most people who work</h6>
+                    <span class="blog-time"><i class="fa fa-clock-o"></i> Jan 02, 2019</span>
+                  </Link>
+                  <Link to="#" class="fb-item">
+                    <h6>Freelance Design Tricks How </h6>
+                    <span class="blog-time"><i class="fa fa-clock-o"></i> Jan 02, 2019</span>
+                  </Link>
+                  <Link to="#" class="fb-item">
+                    <h6>have a computer at home have had </h6>
+                    <span class="blog-time"><i class="fa fa-clock-o"></i> Jan 02, 2019</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-2">
+              <div class="footer-widget">
+                <h5>Program</h5>
+                <ul class="workout-program">
+                  <li><Link to="#">Bodybuilding</Link></li>
+                  <li><Link to="#">Running</Link></li>
+                  <li><Link to="#">Streching</Link></li>
+                  <li><Link to="#">Weight Loss</Link></li>
+                  <li><Link to="#">Gym Fitness</Link></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="footer-widget">
+                <h5>Get Info</h5>
+                <ul class="footer-info">
+                  <li>
+                    <i class="fa fa-phone"></i>
+                    <span>Phone:</span>
+                    (12) 345 6789
+                  </li>
+                  <li>
+                    <i class="fa fa-envelope-o"></i>
+                    <span>Email:</span>
+                    Colorlib.info@gmail.com
+                  </li>
+                  <li>
+                    <i class="fa fa-map-marker"></i>
+                    <span>Address</span>
+                    Iris Watson, Box 283 8562, NY
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
