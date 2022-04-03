d904479cd252ed980c11d35a5d5c7d720fda1d32
Trofimov Max <trofimovmaxdev@gmail.com>
2022-04-03 10:49:39 +0300

    add makeup scheduling table in new component SchedulingTable

--- react-app/src/pages/SchedulePage.jsx
+++ react-app/src/pages/SchedulePage.jsx
@@ -1,13 +1,41 @@
-import {Header} from '../Components/Header';
+import { Header } from '../Components/Header';
+import {SchedulingTable} from '../Components/SchedulingTable';
 
 function SchedulePage() {
     return (
         <>
             <Header>
-                
+
             </Header>
+            {/* Class Time Section Begin */}
+            <section className="classtime-section class-time-table spad">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            <div className="section-title">
+                                <h2>Таблица с расписанием</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="timetable-controls">
+                                <ul>
+                                    <li className="active"> Зал №1 </li>
+                                    <li> Зал №4 </li>
+                                    <li> Зал №5 </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <SchedulingTable>
+                        
+                    </SchedulingTable>
+                </div>
+            </section>
+            {/* Class Time Section End */}
         </>
     );
 }
 
-export {SchedulePage};
+export { SchedulePage };
