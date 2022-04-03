c5758fe48f0ba1aa507af276fa202e2f34a1e581
Trofimov Max <trofimovmaxdev@gmail.com>
2022-04-03 09:42:59 +0300

    move header from App to new Component HeaderHome

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,13 @@
+import { Routes, Route, Link } from 'react-router-dom';
+//import styled, { css } from 'styled-components';
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // get our fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faVk } from "@fortawesome/free-brands-svg-icons";
 
-//import styled, { css } from 'styled-components';
+
 import logo from './img/logo-sfedu.png';
-import { Routes, Route, Link } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
@@ -14,41 +15,15 @@ import { ContactsPage } from './pages/ContactsPage';
 import { NewsPage } from './pages/NewsPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { SchedulePage } from './pages/SchedulePage';
+import {HeaderHome} from './Components/HeaderHome';
 
 function App() {
   return (
     <>
-      <header className="header-section">
+      <HeaderHome>
 
-        <div className="container-fluid">
-          <div className="logo">
-            <Link to="/">
-              <img src={logo} alt="Logo" />
-            </Link>
-          </div>
-          <div className="top-social">
-            <a href="https://vk.com/club101308251"><FontAwesomeIcon icon={faVk} /></a>
-            <a href="https://www.instagram.com/ssc_sfedu/"><FontAwesomeIcon icon={faInstagram} /></a>
-          </div>
-          <div className="container">
-            <div className="nav-menu">
-              <nav className="mainmenu mobile-menu">
-                <ul>
-                  <li><Link className="active" to="/"> Home </Link></li>
-                  <li><Link to="/about"> About </Link></li>
-                  <li><Link to="/schedule"> Schedule </Link></li>
-                  <li><Link to="/news"> News </Link></li>
-                  <li><Link to="/contacts"> Contacts </Link></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-          <div className="mobile-menu-wrap">
-
-          </div>
-        </div>
-      </header>
-
+      </HeaderHome>
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
