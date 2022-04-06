42924a6bb1df083302d0bfbc083e0f7071061264
Trofimov Max <trofimovmaxdev@gmail.com>
2022-04-06 18:47:57 +0300

    add render without API schedule table

--- react-app/src/Components/SchedulingTable.jsx
+++ react-app/src/Components/SchedulingTable.jsx
@@ -1,5 +1,22 @@
+import { Button } from 'react-bootstrap';
 
-function SchedulingTable() {
+function SchedulingTable({ numGym4, numGym5 }) {
+    const isFull = false; // удалить нах ибо это ты будешь получать с сервера
+    // Что может быть лучше шести пятниц на неделе? только 6 воскресений
+    // этот массив мб нах не нужен, но если не хочешь переделывать цикл можешь оставить
+    const dayWeekend = ['Пятница', 'Пятница', 'Пятница', 'Пятница', 'Пятница', 'Пятница'];
+    // этот массив удобен, можно легко заполнить самый левый столбец с примерными промежутками
+    // хз нужны ли они вообще?  
+    const timeClasses = ['9:00 - 10:30', '10:30 - 12:00',
+        '12:00 - 13:30', '13:30 - 15:00', '15:00 - 16:30', '16:30 - 18:00',
+        '18:00 - 19:30', '19:30 - 21:00'];
+    if (numGym4 === 'active') {
+        //query request на получение таблицы зала 4 (schedule_class === '4')
+    }
+    else {
+        //query request на получение таблицы зала 5 (schedule_class === '5')
+    }
+
     return (
         <>
             <div className="row">
@@ -17,119 +34,44 @@ function SchedulingTable() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="workout-time">10.00</td>
-                                <td className="hover-bg ts-item" data-tsmeta="crossfit">
-                                    <span>10.00 - 14.00</span>
-                                    <h6>Crossfit lv1</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="crossfit">
-                                    <span>10.00 - 15.00</span>
-                                    <h6>Crossfit lv1</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="lunge">
-                                    <span>10.00 - 13.00</span>
-                                    <h6>Lunge Ball Bur</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="lunge">
-                                    <span>10.00 - 13.30</span>
-                                    <h6>Lunge Ball Bur</h6>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="workout-time">14.00</td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="lunge">
-                                    <span>14.00 - 17.00</span>
-                                    <h6>Lunge Ball Bur</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="crossfit">
-                                    <span>14.00 - 17.00</span>
-                                    <h6>Crossfit lv1</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="walls">
-                                    <span>14.00 - 15.30</span>
-                                    <h6>Walls to Knees</h6>
-                                </td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td className="workout-time">16.00</td>
-                                <td className="hover-bg ts-item" data-tsmeta="lunge">
-                                    <span>16.00 - 18.00</span>
-                                    <h6>Lunge Ball Bur</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="candy">
-                                    <span>16.00 - 19.00</span>
-                                    <h6>Candy</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="candy">
-                                    <span>16.00 - 19.00</span>
-                                    <h6>Candy</h6>
-                                </td>
-                                <td className="hover-bg ts-item" data-tsmeta="ppsr">
-                                    <span>16.00 - 17.00</span>
-                                    <h6>Ppsr</h6>
-                                </td>
-                                <td className="hover-bg ts-item" data-tsmeta="murph">
-                                    <span>16.00 - 20.00</span>
-                                    <h6>murph</h6>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="workout-time">18.00</td>
-                                <td className="hover-bg ts-item" data-tsmeta="walls">
-                                    <span>18.00 - 20.00</span>
-                                    <h6>Walls to Knees</h6>
-                                </td>
-                                <td className="hover-bg ts-item" data-tsmeta="ppsr">
-                                    <span>18.00 - 20.00</span>
-                                    <h6>ppsr</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="chelsea">
-                                    <span>18.00 - 22.00</span>
-                                    <h6>Chelsea</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="annie">
-                                    <span>18.00 - 22.00</span>
-                                    <h6>annie</h6>
-                                </td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td className="workout-time">20.00</td>
-                                <td className="hover-bg ts-item" data-tsmeta="lunge">
-                                    <span>21.00 - 23.00</span>
-                                    <h6>Lunge Ball Bur</h6>
-                                </td>
-                                <td className="hover-bg ts-item" data-tsmeta="walls">
-                                    <span>20.00 - 22.00</span>
-                                    <h6>Walls to Knees</h6>
-                                </td>
-                                <td className="hover-bg ts-item" data-tsmeta="walls">
-                                    <span>20.30 - 23.00</span>
-                                    <h6>Walls to Knees</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="crossfit">
-                                    <span>22.00 - 23.00</span>
-                                    <h6>Crossfit Lv2</h6>
-                                </td>
-                                <td></td>
-                                <td className="hover-bg ts-item" data-tsmeta="crossfit">
-                                    <span>21.00 - 23.00</span>
-                                    <h6>Crossfit Lv2</h6>
-                                </td>
-                            </tr>
+                            {
+                                // тут создаётся строка где первый столбец соответсвует масиву timeClasses
+                                 timeClasses.map((time) => {
+                                    return (
+                                        <tr> 
+                                            {/* самый левый столбец с промежутками массива timeClasses */}
+                                            <td className="workout-time">{time}</td>
+                                            {
+                                                dayWeekend.map((day) => {
+                                                    // здесь важное условие рендеринга, если у нас нет занятия на это время,
+                                                    // то в блоке else есть рендеринг пустой клетки
+                                                    if (true) {
+                                                        return (
+                                                            <>
+                                                                <td className="hover-bg ts-item">
+                                                                    <span>{time}</span>
+                                                                    {/* количестов народа 10/15 15/15 и так далее*/}
+                                                                    <h6>количество народа?</h6>
+                                                                    {/* условие рендеринга кнопки ЗАПИСАТЬСЯ по условию, что ты получишь из query.response*/}
+                                                                    {!isFull ? <Button variant="outline-light">ЗАПИСАТЬСЯ</Button> : <div></div>}
+                                                                </td>
+                                                            </>
+                                                        )
+                                                    }
+                                                    else {
+                                                        return (
+                                                            <>
+                                                                <td></td>
+                                                            </>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                        </tr>
+
+                                    )
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
--- react-app/src/pages/SchedulePage.jsx
+++ react-app/src/pages/SchedulePage.jsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 import { Header } from '../Components/Header';
 import {SchedulingTable} from '../Components/SchedulingTable';
 
+
 function SchedulePage() {
+    // два веб хука показывают, какая таблица выбрана,
+    //  активной странице применяется стиль 
+    const [numGym4, setNumGym4] = useState('active');
+    const [numGym5, setNumGym5] = useState('');
+
+    function changeNumberGym() {
+        if(numGym4 === 'active'){
+            setNumGym4('');
+            setNumGym5('active');
+        }
+        else {
+            setNumGym5('');
+            setNumGym4('active');
+        }
+      }
+
     return (
         <>
             <Header>
@@ -21,13 +40,17 @@ function SchedulePage() {
                         <div className="col-lg-12">
                             <div className="timetable-controls">
                                 <ul>
-                                    <li className="active"> Зал №4 </li>
-                                    <li> Зал №5 </li>
+                                    <li className={numGym4} onClick={changeNumberGym}> Зал №4 </li>
+                                    <li className={numGym5} onClick={changeNumberGym}> Зал №5 </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <SchedulingTable>
+                    {/* выбранную страницу прокидываю в пропсы, чтобы потом её отрендерить */}
+                    <SchedulingTable 
+                    numGym4 = {numGym4}
+                    numGym5 = {numGym5}
+                    >
                         
                     </SchedulingTable>
                 </div>
